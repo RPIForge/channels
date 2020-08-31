c93dd99ac0579c7784b2081cb46224f0fc6b636f
Michael Honaker <root@forge-dev.2n4ybsij32te5lxxi4i2uxnqpa.bx.internal.cloudapp.net>
2020-08-31 18:18:12 +0000

    Updated requirements and tasks for celery

--- chat/tasks.py
+++ chat/tasks.py
@@ -4,11 +4,11 @@ from celery.decorators import periodic_task
 from datetime import datetime, timedelta
 from .models import FileLog
 
-@periodic_task(run_every=(crontab(minute='*')), name="remove_old_media", ignore_result=True)
+@periodic_task(run_every=(crontab(hour='0')), name="remove_old_media", ignore_result=True)
 def remove_media():
     print("Removing Media")
     
-    time_threshold = datetime.now() - timedelta(hours=1)
+    time_threshold = datetime.now() - timedelta(weeks=1)
     item_list = FileLog.objects.filter(created__lte=time_threshold)
         
     print(time_threshold)
