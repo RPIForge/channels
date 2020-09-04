040d3a5545d6327fa3869cf5e0dd84ca0be5aac8
Michael Honaker <mchonaker@gmail.com>
2020-09-04 05:35:05 +0000

    Finalized form update and chat css
    I fixed the issue where the css on the form was
    not correctly aligned. I also fixed a css issue
    where the chat upload and send buttons were not
    the right size. Finally I centered the data
    in the table columns inline with user feedback.
    
    I realize this took longer than my previous things
    and that is because I absolutly stink at css.
    
    :'( I hate css

--- chat/forms.py
+++ chat/forms.py
@@ -4,36 +4,37 @@ from . import models
 
 class InfoForm(forms.Form):
     request_options =(
-    ('3d printing', 'Id like to come in and Print something'),
-    ('laser cutting', 'Id like to come in and Cut something'),
-    ('electronics', 'Id like to come in and Solder Something'),
-    ('sewing', 'Id like to come in and Sew Something'),
-    ('scanning', 'Id like to come in and Scan something'),
-    ('cad help', 'I have a CAD Model Question or Concern'),
-    ('file optimization', 'How can i optimize my model/cut plan?'),
-    ('general question', 'I have a general question about the forge'),
-    ('hours', 'What are your hours'),
-    ('covid', 'How are you responding to COVID'),
+    ("3d printing", "Id like to come in and Print something"),
+    ("laser cutting", "Id like to come in and Cut something"),
+    ("electronics", "Id like to come in and Solder something"),
+    ("sewing", "Id like to come in and Sew something"),
+    ("scanning", "Id like to come in and Scan something"),
+    ("file optimization", "Id like to optimize my model/cut plan"),
+    ("cad help", "I have a CAD model question or concern"),
+    ("charges", "I have a question about pricing"),
+    ("covid", "I have a question about your COVID response"),
+    ("general question", "I have a general question about the forge"),
+
     )
     
-    name = forms.CharField(label='Your name')
+    name = forms.CharField(label="Your name")
     email = forms.EmailField()
     request = forms.MultipleChoiceField(choices = request_options) 
     
     
     def __init__(self, *args, **kwargs):
         super(InfoForm, self).__init__(*args, **kwargs)
-        self.fields['name'].required = True
-        self.fields['email'].required = True
-        self.fields['request'].required = False
+        self.fields["name"].required = True
+        self.fields["email"].required = True
+        self.fields["request"].required = False
         
         
-        self.fields['name'].label = ""
-        self.fields['email'].label = ""
-        self.fields['request'].label = "" 
+        self.fields["name"].label = ""
+        self.fields["email"].label = ""
+        self.fields["request"].label = "" 
         
-        self.fields['name'].widget.attrs = {"placeholder":"Name"}
-        self.fields['email'].widget.attrs = {"placeholder":"Email"}
+        self.fields["name"].widget.attrs = {"placeholder":"Name"}
+        self.fields["email"].widget.attrs = {"placeholder":"Email"}
         
 
             
@@ -41,10 +42,10 @@ class InfoForm(forms.Form):
 class FileForm(forms.ModelForm):  
     def __init__(self, *args, **kwargs):
         super(FileForm, self).__init__(*args, **kwargs)
-        self.fields['file'].label = ""
-        self.fields['file'].widget.attrs = {"placeholder":"Upload File"}
+        self.fields["file"].label = ""
+        self.fields["file"].widget.attrs = {"placeholder":"Upload File"}
         
     
     class Meta:                                 
         model = models.FileLog 
-        fields = ('file',)
+        fields = ("file",)
