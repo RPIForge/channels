b44a79f7a583b0a9b732c976593da93d5f64a991
Michael Honaker <honakm@forge-dev.2n4ybsij32te5lxxi4i2uxnqpa.bx.internal.cloudapp.net>
2020-09-04 16:54:22 +0000

    Updated urls to server staticfiles

--- forge/urls.py
+++ forge/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # mysite/urls.py
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
 
 urlpatterns = [
     path('', include('chat.urls')),
-    
+    url(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
