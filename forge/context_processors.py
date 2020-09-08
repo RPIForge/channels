f6a01ebae64b5873c4c73ef1f96284d3fe27526f
Michael Honaker <honakm@forge-dev.2n4ybsij32te5lxxi4i2uxnqpa.bx.internal.cloudapp.net>
2020-09-08 00:04:42 +0000

    Updated context processors

--- forge/context_processors.py
+++ forge/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import settings # import the settings file
 
 def websocket_url(request):
-    if (settings.CHAT_SITE_WSS_PORT!=0):
+    if (hasattr(settings, 'CHAT_SITE_WSS_PORT')):
         url="wss://"+settings.CHAT_SITE_URL+":"+settings.CHAT_SITE_WSS_PORT
     else:
         url="ws://"+settings.CHAT_SITE_URL+":"+settings.CHAT_SITE_WS_PORT
